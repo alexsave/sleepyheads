49ce8fc2de349461451d777306075d988b26a769
Alex Saveliev <alex@alexs-mbp.lan>
2022-10-08 15:39:54 -0700

    modified node_module code to get core, deep, rem, and awake sleep values

--- App.js
+++ App.js
@@ -6,116 +6,138 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import type {Node} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+    Colors,
 } from 'react-native/Libraries/NewAppScreen';
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+import AppleHealthKit, {
+    HealthValue,
+    HealthKitPermissions
+} from 'react-native-health';
+import {Words} from './src/Components/Basic/Words';
+import {processSleep} from './src/Utils/ProcessSleep';
+
+
+const getSleepPermissions = cb => {
+    const permissions = {
+        permissions: {
+            read: [AppleHealthKit.Constants.Permissions.SleepAnalysis]
+        }
+    };
+    AppleHealthKit.initHealthKit(permissions, error => {
+        if (error)
+            console.log(error);
+        else
+            cb();
+    });
 };
 
+
+const getSleep = cb => {
+    const startDate = new Date(Date.now() - 7*24*60*60*1000).toISOString();
+    const options = {
+        startDate,
+        ascending: true
+    };
+    AppleHealthKit.getSleepSamples(options, (error, results) => {
+
+        if (error) console.log(error);
+        //console.log(results);
+        cb(results);
+    })
+}
+
+/*
+adding chagnes from
+/https://github.com/agencyenterprise/react-native-health/pull/247
+to fix sleep
+ */
+
 const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+    const [sleepData, setSleepData] = useState([]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+    const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View style={{backgroundColor: 'red'}}>
-          <Text>Hello</Text>
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        height: '100%'
+    };
 
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    useEffect(() => {
+        getSleep(sd => setSleepData(processSleep(sd)));
+
+    }, []);
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar
+                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                backgroundColor={backgroundStyle.backgroundColor}
+            />
+            <ScrollView
+                contentInsetAdjustmentBehavior="automatic"
+                style={backgroundStyle}>
+                <View style={{backgroundColor: 'red'}}>
+                    <Text>Hello</Text>
+                </View>
+                <Words>Step One: Just get the sleep data and display it</Words>
+                {
+                    sleepData.map(sample => {
+                            const something = (new Date(sample.endDate) - new Date(sample.startDate)) / 1000 / 100;
+                            const pos = (new Date(sample.startDate) - new Date(2022, 8, 29))/1000/ 100;
+                            //const pos = new Date(sample.startDate).getTime();
+                            //const pos = new Date(2022, 9, 25).getTime();
+                            return <View style={{
+                                position: 'absolute',
+                                top: pos,
+                                //borderWidth: 1,
+                                borderColor: 'blue',
+                                backgroundColor: sample.value === 'UNKNOWN' ? 'black' : sample.value === 'ASLEEP' ? 'blue': 'green',
+                                zIndex: sample.value === 'INBED' ? -1: 1,
+                                height: something,
+                                key: sample.id
+                            }}>
+                                <Words>{JSON.stringify({...sample})}</Words>
+                                <Words>Height {something}</Words>
+                                <Words>Pos {pos}</Words>
+                            </View>
+                        }
+                    )
+                }
+            </ScrollView>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    sectionContainer: {
+        marginTop: 32,
+        paddingHorizontal: 24,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: '600',
+    },
+    sectionDescription: {
+        marginTop: 8,
+        fontSize: 18,
+        fontWeight: '400',
+    },
+    highlight: {
+        fontWeight: '700',
+    },
 });
 
 export default App;
