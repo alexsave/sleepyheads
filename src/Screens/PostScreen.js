6c221c79b4218511733b18ea6a27d0623cba8786
Alex Saveliev <alexvsaveliev@gmail.com>
2022-12-07 21:52:46 -0800

    postscreen is more built out, comments on now

--- src/Screens/PostScreen.js
+++ src/Screens/PostScreen.js
@@ -1,5 +1,5 @@
 import { useNavigation } from '@react-navigation/native';
-import { StyleSheet, SafeAreaView, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, SafeAreaView, TextInput, TouchableOpacity, View, FlatList } from 'react-native';
 import { useContext, useEffect, useState } from 'react';
 import { BACKGROUND, PRIMARY, TEXT_COLOR } from '../Values/Colors';
 import { Sample } from '../Components/Feed/Sample';
@@ -8,6 +8,18 @@ import { Row } from '../Components/Basic/Row';
 import { Words } from '../Components/Basic/Words';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { UserContext } from '../Providers/UserProvider';
+import UserImage from '../Components/Profile/UserImage';
+
+const Comment = ({comment}) => <Row
+  style={{alignItems: 'center'}}
+  key={comment.id}
+>
+  <UserImage imageKey={comment.user.image} userID={comment.userID} size={50} />
+  <View style={{flex: 1, alignItems: 'center'}}>
+    <Words>{comment.content}</Words>
+  </View>
+</Row>
+
 
 // look up shared transitions, that's the ideal behavior for this
 export const PostScreen = props => {
@@ -56,54 +68,60 @@ export const PostScreen = props => {
           <Words><Ionicons name={'close-outline'} size={30}/></Words>
         </TouchableOpacity>
 
-          <TouchableOpacity
-            style={{flex: 1, height: '100%', right: 0, maxWidth: 80, alignItems: 'center', borderBottomLeftRadius: 10, justifyContent: 'center', backgroundColor: PRIMARY }}
-            onPress={() => {
-              if(!dirty)
-                return;
-              // upload or update
-              uploadSleep({...sleep, title, description})
-                .then(navigation.goBack)
-            }}
-          >
-            <Words style={{fontSize: 20, color: dirty? TEXT_COLOR : 'gray'}}>Save</Words>
-          </TouchableOpacity>
+        <TouchableOpacity
+          style={{flex: 1, height: '100%', right: 0, maxWidth: 80, alignItems: 'center', borderBottomLeftRadius: 10, justifyContent: 'center', backgroundColor: PRIMARY }}
+          onPress={() => {
+            if(!dirty)
+              return;
+            // upload or update
+            uploadSleep({...sleep, title, description})
+              .then(navigation.goBack)
+          }}
+        >
+          <Words style={{fontSize: 20, color: dirty? TEXT_COLOR : 'gray'}}>Save</Words>
+        </TouchableOpacity>
       </Row>
 
-      <TextInput
-        onChangeText={t => {
-          if(!ownPost)
-            return;
-          setDirty(true);
-          setTitle(t);
-        }}
-        value={title}
-        style={{height: 50, fontSize: 40, color: TEXT_COLOR}}
-        placeholder={'Title'}
-      />
 
-      <Sample duration={sleep.data.duration} session={sleep.data}/>
 
-      <TextInput
-        onChangeText={t => {
-          if(!ownPost)
-            return;
-          setDirty(true);
-          setDesc(t);
-        }}
-        value={description}
-        style={{height: 200, color: TEXT_COLOR}}
-        placeholder={'Notes'}
-      />
+      <FlatList
+        data={sleep.comments.items}
+        renderItem={({item, index}) => {
+          if(!index){
+            return <View>
+              <TextInput
+                onChangeText={t => {
+                  if(!ownPost)
+                    return;
+                  setDirty(true);
+                  setTitle(t);
+                }}
+                value={title}
+                style={{height: 50, fontSize: 40, color: TEXT_COLOR}}
+                placeholder={'Title'}
+              />
 
-      {
-        sleep.comments.items.map(comment => {
+              <Sample duration={sleep.data.duration} session={sleep.data}/>
 
-          return <View key={comment.id}>
-            <Words>{JSON.stringify(comment)}</Words>
-          </View>
-        })
-      }
+              <TextInput
+                onChangeText={t => {
+                  if(!ownPost)
+                    return;
+                  setDirty(true);
+                  setDesc(t);
+                }}
+                value={description}
+                style={{height: 200, color: TEXT_COLOR}}
+                placeholder={'Notes'}
+              />
+
+              <Comment comment={item}/>
+
+            </View>
+
+          }
+          return <Comment comment={item}/>;
+        }}/>
 
 
     </View>
