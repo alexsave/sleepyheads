5f96d596494eb3c971dd48fe7b69770991a8118a
Alex Saveliev <alexvsaveliev@gmail.com>
2022-12-30 10:30:53 -0800

    tweaked the notifications message, we'll see if it works later

--- ios/Background.swift
+++ ios/Background.swift
@@ -29,26 +29,26 @@ class Background: NSObject {
     healthKitStore.requestAuthorization(toShare: write, read: read) { granted, error in
       if let error = error {
         print(error)
-      } else {
+        return
+      }
+      
+      center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+        if let error = error {
+          print(error)
+          return
+        }
         
-        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-          
-          if let error = error {
-            print(error)
-          } else {
-            let query: HKObserverQuery = HKObserverQuery(sampleType: self.sampleType, predicate: nil, updateHandler: self.sleepChangedHandler)
-            self.healthKitStore.execute(query)
+        let query: HKObserverQuery = HKObserverQuery(sampleType: self.sampleType, predicate: nil, updateHandler: self.sleepChangedHandler)
+        self.healthKitStore.execute(query)
+        
+        self.healthKitStore.enableBackgroundDelivery(for: self.sampleType, frequency: HKUpdateFrequency.immediate) { (success: Bool, error: Error!) in
+          if (success) {
+            print("Enabled background delivery of step count")
             
-            self.healthKitStore.enableBackgroundDelivery(for: self.sampleType, frequency: HKUpdateFrequency.immediate) { (success: Bool, error: Error!) in
-              if (success) {
-                print("Enabled background delivery of step count")
-                
-              } else {
-                if let theError = error{
-                  print("Failed to enable background delivery of step count. ")
-                  print("Error = \(theError)")
-                }
-              }
+          } else {
+            if let theError = error{
+              print("Failed to enable background delivery of step count. ")
+              print("Error = \(theError)")
             }
           }
         }
@@ -66,8 +66,6 @@ class Background: NSObject {
       print("Error = \(theError)")
     }
     
-    
-    
     // Run the query.
     // Actually query the stepcount changes
     let sampleQuery: HKSampleQuery = HKSampleQuery(sampleType: sampleType,
@@ -80,13 +78,10 @@ class Background: NSObject {
         print("Query results handler error = \(error)")
       }
       
-      
-      
       // note: the query works fine
       // Process the samples here.
       // Send notification
       let content = UNMutableNotificationContent()
-      content.title = "New sleep available"
       
       let casted = samples! as! [HKCategorySample]
       
@@ -106,12 +101,29 @@ class Background: NSObject {
       if (x > lastFound) {
         defaults.set(x, forKey: "lastSleepFound")
         
-        // Can we wake up the app and save to async storage from here?
+        // also will be different content if auto upload
+        let autoU: Bool = defaults.bool(forKey: "autoUploadSleep")
+        
+        if (autoU) {
+          content.title = "New sleep available for upload"
+        } else {
+          content.title = "New sleep uploaded"
+        }
+        
+        // Can we wake up the app and save to async storage from here? Yes, we do
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM-dd-yyyy HH:mm"
+        dateFormatter.locale = .current
+        let startD = dateFormatter.string(from: lastInbedSample.startDate)
+        let endD = dateFormatter.string(from : lastInbedSample.endDate)
         
         // add and notify
-        content.body = "\(lastInbedSample.value) type from \(lastInbedSample.startDate.description(with: .current)) to \(lastInbedSample.endDate.description(with: .current)) from thread \(Thread.current)"
+        // no I don't like this
+        content.body = "Sleep from \(startD) to \(endD)"
+        
+        // you upload from the feed screen if it's the most recent
         if #available(iOS 13.0, *) {
-          content.targetContentIdentifier = "importScreen"
+          content.targetContentIdentifier = "feedScreen"
         }
         
         let uuidString = UUID().uuidString
@@ -127,12 +139,6 @@ class Background: NSObject {
         
       }
       //else dw about it
-      
-      
-      
-      
-      
-      
       
     }
     healthKitStore.execute(sampleQuery)
